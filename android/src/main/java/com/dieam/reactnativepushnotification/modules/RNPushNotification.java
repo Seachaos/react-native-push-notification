e3cdedb42463375529902cc443ac162c80bc8da0
Seachaos <ye.jian.yin@gmail.com>
2016-10-27 00:35:10 +0800

    hook for firebase

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
@@ -7,8 +7,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.dieam.reactnativepushnotification.helpers.ApplicationBadgeHelper;
 import com.facebook.react.bridge.ActivityEventListener;
@@ -21,17 +21,22 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 public class RNPushNotification extends ReactContextBaseJavaModule implements ActivityEventListener {
     public static final String LOG_TAG = "RNPushNotification";// all logging should use this tag
 
     private RNPushNotificationHelper mRNPushNotificationHelper;
     private final Random mRandomNumberGenerator = new Random(System.currentTimeMillis());
-    private RNPushNotificationJsDelivery mJsDelivery;
 
     public RNPushNotification(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -39,12 +44,12 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
         reactContext.addActivityEventListener(this);
 
         Application applicationContext = (Application) reactContext.getApplicationContext();
-        // The @ReactNative methods use this
         mRNPushNotificationHelper = new RNPushNotificationHelper(applicationContext);
-        // This is used to delivery callbacks to JS
-        mJsDelivery = new RNPushNotificationJsDelivery(reactContext);
+        new RNPushNotificationRegistrationService();
 
         registerNotificationsRegistration();
+        registerNotificationsReceiveNotification();
+        registerNotificationsRemoteFetch();
     }
 
     @Override
@@ -59,12 +64,22 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
         return constants;
     }
 
+    private void sendEvent(String eventName, Object params) {
+        ReactContext reactContext = getReactApplicationContext();
+
+        if (reactContext.hasActiveCatalystInstance()) {
+            reactContext
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit(eventName, params);
+        }
+    }
+
     public void onNewIntent(Intent intent) {
         if (intent.hasExtra("notification")) {
             Bundle bundle = intent.getBundleExtra("notification");
             bundle.putBoolean("foreground", false);
             intent.putExtra("notification", bundle);
-            mJsDelivery.notifyNotification(bundle);
+            notifyNotification(bundle);
         }
     }
 
@@ -78,9 +93,66 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
                 WritableMap params = Arguments.createMap();
                 params.putString("deviceToken", token);
 
-                mJsDelivery.sendEvent("remoteNotificationsRegistered", params);
+                sendEvent("remoteNotificationsRegistered", params);
             }
         }, intentFilter);
+
+        startThreadForGetTokenFromFirebase();
+    }
+
+    private void startThreadForGetTokenFromFirebase() {
+        new Thread(new Runnable() {
+            private void tryGetTokenFromFirebase() {
+                try {
+                    Thread.sleep( 3000 );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String token = FirebaseInstanceId.getInstance().getToken();
+                WritableMap params = Arguments.createMap();
+                params.putString("deviceToken", token);
+                sendEvent("remoteNotificationsRegistered", params);
+            }
+            @Override
+            public void run() {
+                for( int ax=0; ax < 3 ; ax++ ) {
+                    tryGetTokenFromFirebase();
+                }
+            }
+        }).start();
+    }
+
+    private void registerNotificationsReceiveNotification() {
+        IntentFilter intentFilter = new IntentFilter(getReactApplicationContext().getPackageName() + ".RNPushNotificationReceiveNotification");
+        getReactApplicationContext().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                notifyNotification(intent.getBundleExtra("notification"));
+            }
+        }, intentFilter);
+    }
+
+    private void registerNotificationsRemoteFetch() {
+        IntentFilter intentFilter = new IntentFilter(getReactApplicationContext().getPackageName() + ".RNPushNotificationRemoteFetch");
+        getReactApplicationContext().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Bundle bundle = intent.getBundleExtra("notification");
+                String bundleString = convertJSON(bundle);
+                WritableMap params = Arguments.createMap();
+                params.putString("dataJSON", bundleString);
+                sendEvent("remoteFetch", params);
+            }
+        }, intentFilter);
+    }
+
+    private void notifyNotification(Bundle bundle) {
+        String bundleString = convertJSON(bundle);
+
+        WritableMap params = Arguments.createMap();
+        params.putString("dataJSON", bundleString);
+
+        sendEvent("remoteNotificationReceived", params);
     }
 
     private void registerNotificationsReceiveNotificationActions(ReadableArray actions) {
@@ -96,7 +168,7 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
                 Bundle bundle = intent.getBundleExtra("notification");
 
                 // Notify the action.
-                mJsDelivery.notifyNotificationAction(bundle);
+                notifyNotificationAction(bundle);
 
                 // Dismiss the notification popup.
                 NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
@@ -104,6 +176,32 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
                 manager.cancel(notificationID);
             }
         }, intentFilter);
+    }
+
+    private void notifyNotificationAction(Bundle bundle) {
+        String bundleString = convertJSON(bundle);
+
+        WritableMap params = Arguments.createMap();
+        params.putString("dataJSON", bundleString);
+
+        sendEvent("notificationActionReceived", params);
+    }
+
+    private String convertJSON(Bundle bundle) {
+        JSONObject json = new JSONObject();
+        Set<String> keys = bundle.keySet();
+        for (String key : keys) {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    json.put(key, JSONObject.wrap(bundle.get(key)));
+                } else {
+                    json.put(key, bundle.get(key));
+                }
+            } catch (JSONException e) {
+                return null;
+            }
+        }
+        return json.toString();
     }
 
     @ReactMethod
@@ -145,7 +243,7 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
             Bundle bundle = intent.getBundleExtra("notification");
             if (bundle != null) {
                 bundle.putBoolean("foreground", false);
-                String bundleString = mJsDelivery.convertJSON(bundle);
+                String bundleString = convertJSON(bundle);
                 params.putString("dataJSON", bundleString);
             }
         }
